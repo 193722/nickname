f04ab80c805a0b67c651e02ce665f7db85a7173f
zheye <zheye@yiji.com>
2018-03-26 23:23:42 +0800

    maven多模块构建项目

---
+++ scholar-assemble/src/main/java/clud/zen/scholar/ScholarAssembleApplication.java
@@ -0,0 +1,12 @@
+package clud.zen.scholar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarAssembleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarAssembleApplication.class, args);
+	}
+}
